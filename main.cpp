02da8b554e301a75e8249605846451070e9c7493
pacodev30 <pacofustier@gmail.com>
2024-11-23 23:09:47 +0100

    Reset project

--- main.cpp
+++ main.cpp
@@ -1,19 +1,6 @@
-#include "Utils/File.h"
-#include <string>
-#include "Utils/struct.h"
 
 int main()
 {
-	const utils::struct_file f1{
-		"/Users/pascal/dev/projects/cpp/workspace/Ressources/data.txt",
-		"Esteban \nStella \nMaria",
-		false
 
-	};
-
-	utils::File::Write(f1.pathFile, f1.dataToWrite);
-	const std::string f1data(utils::File::Read(f1.pathFile));
-
-	std::cout << f1data << std::endl;
 	return 0;
 }
